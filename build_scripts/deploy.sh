9e80fff9b616b84ce38e24fd4fd0b1ec56323607
Nathan Probert <nathanprobert@rogers.com>
2025-03-29 17:43:30 -0400

    oh brother

--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -56,8 +56,8 @@ generate_smartscore_stack() {
       --stack-name "$STACK_NAME" \
       --template-body file://"$TEMPLATE_FILE" \
       --parameters ParameterKey=ENV,ParameterValue="$ENV" \
-        ParameterKey=SUPABASE_URL,ParameterValue="$SUPABASE_URL" \
-        ParameterKey=SUPABASE_API_KEY,ParameterValue="$SUPABASE_API_KEY" \
+        ParameterKey=SupabaseUrl,ParameterValue="$SUPABASE_URL" \
+        ParameterKey=SupabaseApiKey,ParameterValue="$SUPABASE_API_KEY" \
       --capabilities CAPABILITY_NAMED_IAM
 
     echo "Waiting for CloudFormation stack creation to complete..."
