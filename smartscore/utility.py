0037417d43ec90a640a4e9bf9d15a0f233f72b9c
Nathan Probert <nathanprobert@rogers.com>
2025-06-12 22:52:21 -0400

    Corrected templates, may have to do further testing when there is a live game

--- smartscore/utility.py
+++ smartscore/utility.py
@@ -148,7 +148,7 @@ def remove_last_game(time_set):
     return {time.isoformat() for time in time_objects}
 
 
-def exponential_backoff_request(url, method="get", data=None, json_data=None, max_retries=5, base_delay=1):
+def exponential_backoff_request(url, method="get", data=None, json_data=None, max_retries=5, base_delay=5):
     """
     Makes HTTP requests with exponential backoff retry strategy.
 
