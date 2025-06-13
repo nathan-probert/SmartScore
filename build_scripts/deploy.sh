63bc7eb7185ab32b75098bf786213104d7a9a459
Nathan Probert <nathanprobert@rogers.com>
2025-06-12 20:29:16 -0400

    Use my account id

--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -4,7 +4,7 @@
 ENV=${ENV:-dev}  # If ENV is not set, default to "dev"
 
 AWS_REGION=${AWS_REGION:-us-east-1}
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+AWS_ACCOUNT_ID=${{ secrets.AWS_ACCOUNT_ID }}
 
 MAX_ZIP_SIZE_MB=25
 
