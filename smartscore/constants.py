903ed10405d0bff0f4baa8e13b17e15e498e6d19
Nathan Probert <nathanprobert@rogers.com>
2025-10-14 19:55:17 -0400

    Temporairly allocate more weighting to historic gpg to account for new season

--- smartscore/constants.py
+++ smartscore/constants.py
@@ -1,4 +1,5 @@
 from config import ENV
+import make_predictions_rust
 
 DRAFTKINGS_NHL_ID = 42133
 DRAFTKINGS_GOAL_SCORER_CATEGORY = 1190
@@ -19,3 +20,26 @@ HISTORY_API_URL = (
 LAMBDA_API_NAME = f"Api-{ENV}"
 
 DAYS_TO_KEEP_HISTORIC_DATA = 7
+
+# Prediction weights
+# old weights
+# weights = make_predictions_rust.Weights(
+#     gpg=0.3,
+#     five_gpg=0.4,
+#     hgpg=0.3,
+#     tgpg=0.0,
+#     otga=0.0,
+#     hppg_otshga=0.0,
+#     is_home=0.0,
+# )
+
+# For the start of the season, we will temporiarly increase the weight of hgpg
+WEIGHTS = make_predictions_rust.Weights(
+    gpg=0.76-0.3,
+    hgpg=0.06+0.3,
+    five_gpg=0.0,
+    tgpg=0.0,
+    otga=0.04,
+    is_home=0.06,
+    hppg_otshga=0.08,
+)
