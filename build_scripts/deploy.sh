dcf87e30c35a6cff3aae303b104eaa0974bf5d07
Nathan Probert <nathanprobert@rogers.com>
2025-12-03 15:17:28 -0500

    Adjust deploy script

--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -196,7 +196,7 @@ mkdir -p $OUTPUT_DIR
 # update dependencies
 echo "Updating dependencies..."
 poetry export -f requirements.txt --output $OUTPUT_DIR/requirements.txt --without-hashes
-poetry run pip install --no-deps -r $OUTPUT_DIR/requirements.txt -t $OUTPUT_DIR
+pip install --no-deps -r $OUTPUT_DIR/requirements.txt -t $OUTPUT_DIR
 rm -f $OUTPUT_DIR/requirements.txt
 
 # compile C code
